7bea1663597560996e9264a8dab0d65337babcbd
Irene Tung <irene277299@gmail.com>
2022-06-09 10:40:04 +0800

    fix: setup styled-components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Text = styled.div`
+  color: blue;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Text>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </Text>
         <a
           className="App-link"
           href="https://reactjs.org"
