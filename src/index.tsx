6de77eac5d1901f4d26794afe65bfc5b3a27dffd
Irene Tung <irene277299@gmail.com>
2022-06-09 10:41:13 +0800

    fix: setup normalize css.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Normalize } from 'styled-normalize';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,6 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <Normalize />
     <App />
   </React.StrictMode>
 );
